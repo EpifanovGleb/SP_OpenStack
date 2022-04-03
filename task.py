d843127298e40967c29f2369d2ccb93d631be1b9
EpifanovGleb <102690521+EpifanovGleb@users.noreply.github.com>
2022-04-03 20:44:49 +0300

    Add files via upload

--- task.py
+++ task.py
@@ -1,115 +1,136 @@
 import openstack
-from openstack import connection, orchestration
-from keystoneauth1 import loading
-from keystoneauth1 import session as session
-from keystoneclient.v3 import client as client
-from keystoneauth1.identity import v3
-import openstack.compute.v2.limits
-from keystoneauth1 import loading
-from keystoneauth1 import session
 from pprint import pprint
 
 
-def processing(x, lst_1):
-    answer = 0
-    for it in x:
-        if it == lst_1[0]:
-            if type(x[it]) == str:
-                if len(lst_1) == 1:
-                    return x[it]
-                else:
-                    return 0
-            if type(x[it]) == dict:
-                if len(lst_1) > 1:
-                    lst_1.pop(0)
-                    answer = processing(x[it], lst_1)
-                else:
-                    return 0
+def string_parsing(tmp_str):
+    result = tmp_str
+    while True:
+        checker = result.find('-')
+        if checker != -1:
+            result = result.replace("-", "_")  # For example: key-manager -> key_manager
+        else:
+            break
+    return result
 
-        return answer
+
+def processing(tmp_list):
+    list_for_check = ['volume']
+    answer = []
+    for y in tmp_list:
+        answer.append(string_parsing(y))
+
+    for z in list_for_check:
+        for x in tmp_list:
+            if x.find(z):
+                answer.append(z)
+                break
+
+    return answer
 
 
 if __name__ == '__main__':
+    conn = openstack.connect(cloud='openstack', region_name='regionOne')
+    input_first_arg = "images"
+    #input_first_arg = input()
+    # input_second_arg = input()
+    # input_third_arg = input()
+    list_of_available_services = []
 
-    conn = openstack.connect(cloud='openstack', region_name='regionOne')  # Getting an object after call
-# # # Module 1
+    my_dict = conn.service_catalog  # Getting the list of available services
+    for element in my_dict:
+        temp = element.get("type")
+        list_of_available_services.append(temp)
 
-input_first_arg = ""  # The input of a first str
+    list_of_available_services = processing(list_of_available_services)  # Changing the list
+    list_of_available_services.remove("key_manager")
+    print("The list of available services (refactored):\n")
+    print(list_of_available_services, "\n")
 
-# CASE 1
-'''
-flag_1 = hasattr(conn.compute, input_first_arg)  # Checking if attribute exists
-if flag_1:
-    my_dict = getattr(conn.compute, input_first_arg)  # If attribute exists
-    for temp in my_dict():
-        print(temp.name)
+    counter = False
+    #list_of_available_services = ['volumev3', 'baremetal_introspection', 'cloudformation', 'compute', 'image', 'network', 'orchestration', 'baremetal', 'object_store', 'placement', 'identity', 'volumev2', 'volume']
+    list_to_output = []
+    tmp_vec = []
 
-flag_2 = hasattr(conn.identity, input_first_arg)
-if flag_2:
-    my_dict = getattr(conn.identity, input_first_arg)  # If attribute exists
-    for temp in my_dict():
-        print(temp)
+    for elem in list_of_available_services:
+        flag = True
+        if hasattr(conn, elem):
+            my_object = getattr(conn, elem)
+            dict_my = []
+            try:
+                dict_my = getattr(my_object, input_first_arg)
+            except:
+                flag = False
 
-'''
-# CASE 2
-'''
-my_dict = {}
-check_string = "list_" + input_first_arg
-conn.list_stacks()
+            if flag:
+                dict_my = getattr(my_object, input_first_arg)
+                #print(elem)
+                if counter:
+                    tmp_dict = list(list_to_output[0].keys())
+                    i = 0
+                    for it in dict_my():
+                        for el in it:
+                            if list_to_output[i].get(el) == None:
+                                list_to_output[i].update({el: it.get(el)})
 
-flag_3 = hasattr(conn, check_string)
-if flag_3:
-    my_dict = getattr(conn, check_string)
-    pprint(my_dict())
-# # # Module 2 and 3
-my_lst = []
-while True:
-    try:
-        key = input()
-    except:
-        break
-    my_lst.append(key)
-print(my_lst)
-'''
+                        i += 1
+                else:
+                    check = True
+                    try:
+                        for it in dict_my():  # groups
+                            list_to_output.append(dict(it))
+                        counter = True
+                    except:
+                        check = False
 
-lst = [{'a': "success", 'b': {'f': {'e': "success"}, 'c': "fail"}}, {'a': "failure"}]
-inp_second_arg = ['a']
-inp_third_arg = "r"
-answer = ""
+    #print("The result of the first part:\n")
+    #pprint(list_to_output, sort_dicts=False)
 
-for diction in lst:
-    my_diction = diction.copy()
-    my_copy = inp_second_arg.copy()
-    temp = len(inp_second_arg)
-    i = 0
-    while i < temp:
-        it = inp_second_arg[i]
-        try:
-            my_diction[it]
-        except:
-            answer = 0
-            break
-        if i == temp - 1:
-            if type(my_diction[it]) == str:
-                answer = my_diction[it]
+    overall = []
+    #lst = [{'a': "success", 'b': {'f': {'e': "success"}, 'c': "fail"}}, {'a': "failure"}]
+
+    lst = list_to_output
+    inp_second_arg = ['name']
+    inp_third_arg = "2"
+    answer = ""
+    print("The result of the second task:\n")
+
+    for diction in lst:
+        my_diction = diction.copy()
+        my_copy = inp_second_arg.copy()
+        temp = len(inp_second_arg)
+        i = 0
+        while i < temp:
+            it = inp_second_arg[i]
+            try:
+                my_diction[it]
+
+            except:
+                answer = 0
+                break
+
+            if i == temp - 1:
+                if type(my_diction[it]) == str:
+                    answer = my_diction[it]
+
+                else:
+                    answer = 0
+                    break
+
             else:
-                answer = 0
-                break
-        else:
-            if type(my_diction[it]) != dict:
-                answer = 0
-                break
-            my_copy.pop(0)
-            my_diction = my_diction[it]
-        i += 1
-    #print(answer)
-    if type(answer) == str:
-        if answer.find(inp_third_arg) != -1:
-            print(diction)
+                if type(my_diction[it]) != dict:
+                    answer = 0
+                    break
 
+                my_copy.pop(0)
+                my_diction = my_diction[it]
+            i += 1
 
-
-
+        if type(answer) == str:
+            print(answer, end=" ")
+            if answer.find(inp_third_arg) != -1:
+                overall.append(dict(diction))
+    print("\n\nThe result of the task:\n")
+    print(overall)
 
 
 
