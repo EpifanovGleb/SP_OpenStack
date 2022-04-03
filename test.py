74da4936648ba609eff3d72f1af99df9f1664e87
EpifanovGleb <102690521+EpifanovGleb@users.noreply.github.com>
2022-04-03 20:47:40 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,30 +1,44 @@
-def processing(x):
+    overall = []
+    lst = [{'a': "success", 'b': {'f': {'e': "success"}, 'c': "fail"}}, {'a': "failure"}]
+    inp_second_arg = ['a']
+    inp_third_arg = "u"
     answer = ""
-    for it in x:
-        if type(x[it]) != dict:
-            return x[it]
-        else:
-            answer = processing(x[it])
+    print("The result of the second task:\n")
 
-    return answer
+    for diction in lst:
+        my_diction = diction.copy()
+        my_copy = inp_second_arg.copy()
+        temp = len(inp_second_arg)
+        i = 0
+        while i < temp:
+            it = inp_second_arg[i]
+            try:
+                my_diction[it]
 
+            except:
+                answer = 0
+                break
 
-lst = [{'a': {'b': {'c': "fail"}}}, {'c': 'd'}, {'b': {'c': "fail"}}]
+            if i == temp - 1:
+                if type(my_diction[it]) == str:
+                    answer = my_diction[it]
 
-while True:
-    data = dict()
-    try:
-        key = input()
-        value = input()
-    except:
-        break
-    data[key] = value
-    lst.append(data)
+                else:
+                    answer = 0
+                    break
 
-for diction in lst:
-    for i in diction:
-        if type(diction[i]) != dict:
-            result = diction[i]
-        else:
-            result = processing(diction[i])
-        print(result)
+            else:
+                if type(my_diction[it]) != dict:
+                    answer = 0
+                    break
+
+                my_copy.pop(0)
+                my_diction = my_diction[it]
+            i += 1
+
+        if type(answer) == str:
+            print(answer, end=" ")
+            if answer.find(inp_third_arg) != -1:
+                overall.append(dict(diction))
+    print("\n\nThe result of the task:\n")
+    print(overall)
